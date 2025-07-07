9d46a6b6037ab824fba56a77af2dd0ec7f4ac2da
Junaid Khan <junaidsultan92pk@gmail.com>
2025-07-07 12:23:15 +0500

    Logo added

--- app/PartnerUsPortal/layout.tsx
+++ app/PartnerUsPortal/layout.tsx
@@ -3,9 +3,34 @@ import { AuthProvider } from "../(auth)/auth/authProvider";
 import { Navbar } from "./components/Navbar";
 import { Sidebar } from "./components/Sidebar";
 
+// export const metadata: Metadata = {
+//   title: "World Wide Admission",
+//   description: "Your study abroad journey, simplified with AI",
+// };
 export const metadata: Metadata = {
-  title: "World Wide Admission",
+  title: "World Wide Admission Hub",
   description: "Your study abroad journey, simplified with AI",
+  openGraph: {
+    title: "World Wide Admission Hub",
+    description: "Your study abroad journey, simplified with AI",
+    url: "https://www.wwah.ai/",
+    siteName: "World Wide Admission",
+    images: [
+      {
+        url: "https://www.wwah.ai/WWAHlogo.svg",
+        width: 800,
+        height: 600,
+        alt: "WWAH Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "World Wide Admission Hub",
+    description: "Your study abroad journey, simplified with AI",
+    images: ["https://www.wwah.ai/WWAHlogo.svg"],
+  },
 };
 
 export default function PagesLayout({
@@ -30,13 +55,10 @@ export default function PagesLayout({
             <Sidebar />
           </div>
 
-
-
           {/* Main content with margin left to avoid sidebar */}
           <main className="pt-20 p-4 min-h-screen overflow-y-auto lg:ml-60">
             {children}
           </main>
-
         </AuthProvider>
       </body>
     </html>
